2d2b1e127088325ae7ea5f5da2884243781e94c1
Fosntous <43320126+Fosntous@users.noreply.github.com>
2018-09-30 23:00:10 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var i = number, c1 = 0, c2 = 1;
+  while(i > 4){
+    c1+=(Math.floor(i/5));
+    i = Math.floor(i/5);
+    c2*=5;
+  }
+  return c1;
 }
